d4f54c124c6d52494f80a1bce7fff70850ce1701
ipreacher <ipreacher@hotmail.com>
2017-02-11 11:49:15 +0800

    uuid

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Introduction
 
-这里放一些 Python 方面的小技巧的东西。
+这里放一些 Python 方面的小东西。
+
+## add_my_site
+
+图片右下方添加博客地址
+
+## uuid
+
+随机生成 10 条验证码并保存为 txt 文件
---
+++ uuid/uuid.py
@@ -0,0 +1,15 @@
+import uuid
+
+uuids = []
+
+def  Security_code():
+	for i in range(10):
+		uuids.append(uuid.uuid4())
+	print(uuids)
+	f = open('uuids.txt','w')
+	f.write(str(uuids))
+	f.close()
+	return 0
+
+if __name__ == '__main__':
+	Security_code()
