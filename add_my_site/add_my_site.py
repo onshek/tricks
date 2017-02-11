2795ee9b109c19ab31bb12b69558456652fd7559
ipreacher <ipreacher@hotmail.com>
2017-02-11 10:09:16 +0800

    update

--- add_my_site/add_my_site.py
+++ add_my_site/add_my_site.py
@@ -3,7 +3,8 @@ from PIL import Image, ImageDraw, ImageFont
 def add_my_site(img):
 	draw = ImageDraw.Draw(img)
 	width, height = img.size 
-	myfont = ImageFont.truetype('/System/Library/Fonts/Helvetica.dfont', size = 150)
+	size_1 = int(width * 0.0375)
+	myfont = ImageFont.truetype('/System/Library/Fonts/Helvetica.dfont', size = size_1)
 	fillcolor = '#FFFFFF'
 	draw.text((width * 0.55, height * 0.9375), 'https://ipreacher.github.io/', font = myfont, fill = fillcolor)
 	img.save('result.jpg', 'jpeg')
