bd3043c59a87efed4cf6bf26c14170bb291f9dc1
ipreacher <ipreacher@hotmail.com>
2017-02-15 19:32:34 +0800

    convert indentation to spaces

--- data_flow/data_flow.py
+++ data_flow/data_flow.py
@@ -12,21 +12,21 @@ p=lambda:random.choice('`1234567890-=~!@#$%^&*()_+qwertyuiop[]\{}|asdfghjkl;'"zx
 
 
 def run():
-	print('|'.join([p(),p(),p(),p(),p(),p(),p(),p(),p(),p(),p(),p(),p(),p(),p(),p(),
-		p(),p(),p(),p(),p(),p(),p(),p(),p(),p(),p(),p(),p(),p(),p(),p(),p(),p(),p(),
-		p(),p(),p(),p()]),end='\r') 
+    print('|'.join([p(),p(),p(),p(),p(),p(),p(),p(),p(),p(),p(),p(),p(),p(),p(),p(),
+        p(),p(),p(),p(),p(),p(),p(),p(),p(),p(),p(),p(),p(),p(),p(),p(),p(),p(),p(),
+        p(),p(),p(),p()]),end='\r') 
 
 
 if __name__ == '__main__':
-	print('数据流开始执行...\n')
-	while True:
-		try:
-			run()
-			time.sleep(0.05)
-		except KeyboardInterrupt:
-			print('\n'
-				'\n'
-				'数据流已执行完毕！\n'
-        		'更多有意思的小玩意，请戳---->\n'
-        		'[https://github.com/ipreacher/tricks]')
-			break
+    print('数据流开始执行...\n')
+    while True:
+        try:
+            run()
+            time.sleep(0.05)
+        except KeyboardInterrupt:
+            print('\n'
+                '\n'
+                '数据流已执行完毕！\n'
+                '更多有意思的小玩意，请戳---->\n'
+                '[https://github.com/ipreacher/tricks]')
+            break
